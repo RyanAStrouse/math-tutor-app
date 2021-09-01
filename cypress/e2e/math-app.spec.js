a982ba8b31d50045b71f07c39802463faa2ad301
Ryan <ryanastrouse@gmail.com>
2021-09-01 01:03:42 -0400

    Update math-app.spec.js

--- cypress/e2e/math-app.spec.js
+++ cypress/e2e/math-app.spec.js
@@ -1,10 +1,23 @@
 describe('random math app', () => {
     it('can make calculations', () => {
         cy.visit('/')
-        cy.get('.x-value').as('first-value')
-        cy.get('.y-value').as('second-value')
-        cy.get('.mat-button-wrapper')
-        .click()
+        cy.get('.mat-card-content > div').children().eq(0).then(($firstnum) => {
+			const firstvalue = $firstnum.text()
+			cy.log(firstvalue)
+		})
+		//cy.log(firstnum)
+        cy.get('.mat-card-content > div').children().eq(1).then(($secondnum) => {
+			const secondvalue = $secondnum.text()
+			cy.log(secondvalue)
+		})
+		cy.get('.mat-card-content > div').children().eq(2).then(($answer) => {
+			const sum = $firstvalue + $secondvalue
+			this.type(sum)
+		})
+		//cy.log(sum)
+		//cy.get('@answer').type(sum)
+        cy.get('.mat-button-wrapper').as('btn')
+        cy.get('@btn').click()
         .findByTestId('total')
     })
   })
