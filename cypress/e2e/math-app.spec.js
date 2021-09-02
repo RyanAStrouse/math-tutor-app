4018fb8f8bfe7cae20a35cd94aafea7877b359b6
Ryan <ryanastrouse@gmail.com>
2021-09-02 09:15:18 -0400

    Update math-app.spec.js
    Added a couple more tests

--- cypress/e2e/math-app.spec.js
+++ cypress/e2e/math-app.spec.js
@@ -56,4 +56,42 @@ describe('random math app', () => {
 		})
 		cy.get('#toast-container').should('contain', ' Sorry, that is not correct. Please try again.')
     })
+	it('can assert focus after click', () => {
+		var firstnum = 0
+		var secondnum = 0
+        cy.visit('/')
+        cy.get('.mat-card-content span').eq(0).as('firstnum').then(($firstvalue) => {
+			firstnum = $firstvalue.text()
+			cy.log(firstnum)
+			cy.get('.mat-card-content span').eq(1).as('secondnum').then(($secondvalue) => {
+				secondnum = $secondvalue.text()
+				cy.log(secondnum)
+				var sum = parseInt(firstnum) + parseInt(secondnum)
+				cy.log(sum)
+				cy.get('.mat-card-content input').type(sum)
+				cy.get('.mat-button-wrapper').as('btn')
+				cy.get('@btn').click()
+			})
+		})
+		cy.get('.mat-card-content input').should('have.focus')
+    })
+	it('can verify decimal use', () => {
+		var firstnum = 0
+		var secondnum = 0
+        cy.visit('/')
+        cy.get('.mat-card-content span').eq(0).as('firstnum').then(($firstvalue) => {
+			firstnum = $firstvalue.text()
+			cy.log(firstnum)
+			cy.get('.mat-card-content span').eq(1).as('secondnum').then(($secondvalue) => {
+				secondnum = $secondvalue.text()
+				cy.log(secondnum)
+				var sum = parseInt(firstnum) + parseInt(secondnum) + .1
+				cy.log(sum)
+				cy.get('.mat-card-content input').type(sum).then(($answer_input) => {
+					cy.get($answer_input).should('have.attr', 'ng-reflect-model', sum)
+				})
+			})
+		})
+		
+    })
   })
