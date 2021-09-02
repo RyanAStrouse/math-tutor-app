5781d7c089ab20f48af1260224d2a64e5bcc1990
Ryan <ryanastrouse@gmail.com>
2021-09-01 23:52:33 -0400

    Update math-app.spec.js
    additional tests

--- cypress/e2e/math-app.spec.js
+++ cypress/e2e/math-app.spec.js
@@ -17,6 +17,43 @@ describe('random math app', () => {
 				cy.get('@btn').click()
 			})
 		})
+    })
+	it('can verify success messaging', () => {
+		var firstnum = 0
+		var secondnum = 0
+        cy.visit('/')
+        cy.get('.mat-card-content span').eq(0).as('firstnum').then(($firstvalue) => {
+			firstnum = $firstvalue.text()
+			cy.log(firstnum)
+			cy.get('.mat-card-content span').eq(1).as('secondnum').then(($secondvalue) => {
+				secondnum = $secondvalue.text()
+				cy.log(secondnum)
+				var sum = parseInt(firstnum) + parseInt(secondnum)
+				cy.log(sum)
+				cy.get('.mat-card-content input').type(sum)
+				cy.get('.mat-button-wrapper').as('btn')
+				cy.get('@btn').click()
+			})
+		})
 		cy.get('#toast-container').should('contain', 'That\'s right! Try another one.')
+    })
+	it('can verify failure messaging', () => {
+		var firstnum = 0
+		var secondnum = 0
+        cy.visit('/')
+        cy.get('.mat-card-content span').eq(0).as('firstnum').then(($firstvalue) => {
+			firstnum = $firstvalue.text()
+			cy.log(firstnum)
+			cy.get('.mat-card-content span').eq(1).as('secondnum').then(($secondvalue) => {
+				secondnum = $secondvalue.text()
+				cy.log(secondnum)
+				var sum = parseInt(firstnum) + parseInt(secondnum) + 1
+				cy.log(sum)
+				cy.get('.mat-card-content input').type(sum)
+				cy.get('.mat-button-wrapper').as('btn')
+				cy.get('@btn').click()
+			})
+		})
+		cy.get('#toast-container').should('contain', ' Sorry, that is not correct. Please try again.')
     })
   })
